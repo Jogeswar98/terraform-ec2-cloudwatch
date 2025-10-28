a3d4ec48a8c16f927953157e92077030dfa2d823
Jogeswar98 <jogeswarpradhan98@gmail.com>
2025-10-28 13:36:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,18 @@
-provider "aws" {
- region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "tws-portfolio01"  # <-- Replace with your bucket name
+    key    = "ec2-instance/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
