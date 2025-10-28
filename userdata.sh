9e95aa0ba8d8af610cb7c5743b1d26c3c96af116
Jogeswar98 <jogeswarpradhan98@gmail.com>
2025-10-28 17:10:02 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -11,14 +11,16 @@ sudo mkdir -p /usr/share/keyrings
 curl -fsSL https://repo.zabbix.com/zabbix-official-repo.key \
   | sudo gpg --dearmor -o /usr/share/keyrings/zabbix.gpg
 
+# Create the repo list manually
 echo "deb [signed-by=/usr/share/keyrings/zabbix.gpg] https://repo.zabbix.com/zabbix/6.4/ubuntu $(lsb_release -cs) main" \
   | sudo tee /etc/apt/sources.list.d/zabbix.list
 
+# Install Zabbix agent
 sudo apt update -y
-sudo apt install -y zabbix-agent
+sudo apt install -y zabbix-agent || echo "Zabbix install failed!"
 
-sudo systemctl enable zabbix-agent
-sudo systemctl start zabbix-agent
+sudo systemctl enable zabbix-agent || true
+sudo systemctl start zabbix-agent || true
 
 #########################################
 # ☁️ CLOUDWATCH AGENT INSTALLATION
